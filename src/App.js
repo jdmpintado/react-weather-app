b6fa0c3abfc1ab03184d1a8bff71905341bf6600
jdmpintado <137705155+jdmpintado@users.noreply.github.com>
2025-09-05 10:34:23 +0100

    Added link to GitHub repository

--- src/App.js
+++ src/App.js
@@ -2,5 +2,28 @@ import React from "react";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <header>Weather App</header>
+      <footer>
+        This project was coded by{" "}
+        <a
+          href="https://github.com/jdmpintado"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Jessica Pintado
+        </a>{" "}
+        and is{" "}
+        <a
+          href="https://github.com/jdmpintado/react-weather-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          open-sourced on GitHub
+        </a>
+        .
+      </footer>
+    </div>
+  );
 }
